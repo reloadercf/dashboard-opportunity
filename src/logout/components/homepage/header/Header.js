54c0c8e2e7ba2eb9ad72ab0946ed60262da9d238
reloadercfm <elcfm1@gmail.com>
2018-12-13 17:13:02 -0600

    isto

--- src/logout/components/homepage/header/Header.js
+++ src/logout/components/homepage/header/Header.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import android from '../../../../assets/images/header/android.png'
-import ios from '../../../../assets/images/header/aple.png'
+import android from '../../../../assets/images/header/google.png'
+import ios from '../../../../assets/images/header/apple.png'
 import './header_style.css'
 
 
 
 export default () => {
   return (
+    
         <div class="cover">
-          <div class="header contenedor">
-            <img src={android} />
-            <img src={ios}/>
+          <div class='cover_alineacion'>
+            <img width='20%' height='5%' src={android}/>
+            <img width='20%' height='5%' src={ios}/>
           </div>
+            <div class="texto">
+              Descarga la app
+            </div>
         </div>
   )
 }
