52262ed8e5741c021987e57acb4b42649d29e0c3
reloadercfm <elcfm1@gmail.com>
2018-12-17 14:51:38 -0600

    quedo el componente de redactar

--- src/login/dashboard/Dashboard.js
+++ src/login/dashboard/Dashboard.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import Navbar from './navbar/Navbar'
-import Main from './main/Main'
+// import Main from './main/Main'
 import Footer from './footer/Footer'
 import './dashboard_style.css'
-// import Redactar from "./main/layouts/redactar/Redactar";
+import Redactar from "./main/layouts/redactar/Redactar";
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div className='posision'>
         <Navbar/>
-        <Main/>
-        {/* <Redactar/> */}
+        {/* <Main/> */}
+        <Redactar/>
         <Footer/>
       </div>
     )
--- src/login/dashboard/footer/Footer.js
+++ src/login/dashboard/footer/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Layout} from 'antd';
+import { Layout } from "antd";
 
 const { Footer } = Layout;
 export default () => {
   return (
     <div>
-            <Footer style={{ textAlign: 'center', backgroundColor:'#000000',color:'#ffffff'}}>
+            <Footer style={{ textAlign: 'center', backgroundColor:'#000000',color:'#ffffff',position:'relative', bottom:'100'}}>
                 Plan B
             </Footer>
     </div>
